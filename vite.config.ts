27b67699adf33da8f29b296c328dc045b3da7a4b
NhutMai0501 <nhutmai0515@gmail.com>
2025-11-29 14:49:31 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
   // THÊM DÒNG NÀY (Copy chính xác tên repo của bạn):
-  base: "/Machine-Learning-Project-251---CEML1/",
+  base: '/my_site_showcase/',
 
   server: {
     host: "::",
